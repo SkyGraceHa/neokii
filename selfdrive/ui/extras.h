2ccbf70c76e99500f2c0011cfbaf9840c24b46aa
Chanho Jung <jungch@naver.com>
2021-09-13 21:51:47 +0900

    Update extras.h

--- selfdrive/ui/extras.h
+++ selfdrive/ui/extras.h
@@ -95,9 +95,9 @@ static void ui_draw_extras_limit_speed(UIState *s)
 }
 
 static NVGcolor get_tpms_color(float tpms) {
-    if(tpms < 5 || tpms > 60) // N/A
+    if(tpms < 30 || tpms > 45) // N/A
         return nvgRGBA(255, 255, 255, 200);
-    if(tpms < 30)
+    if(tpms < 33 || tpms > 43)
         return nvgRGBA(255, 90, 90, 200);
     return nvgRGBA(255, 255, 255, 200);
 }
